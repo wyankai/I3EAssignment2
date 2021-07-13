a4d2ad6a1d4d175c9c03dc9895f3223f3915f31b
wyankai <73118690+wyankai@users.noreply.github.com>
2021-07-13 23:00:27 +0800

    Add script to reduce lag from rendering portal texture

--- Assets/Scripts/Colliders for the different areas/ThirdAreaCollider.cs
+++ Assets/Scripts/Colliders for the different areas/ThirdAreaCollider.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class ThirdAreaCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // To disable the cameras and planes while in this area so that the game would not lag too much during its runtime(PART 1)
+    public Camera CameraA;
+    public Camera CameraB;
+    public Camera CameraC;
+    public Camera CameraD;
+    public Camera CameraE;
+    public Camera CameraF;
+
+    public GameObject planeA;
+    public GameObject planeB;
+    public GameObject planeC;
+    public GameObject planeD;
+    public GameObject planeE;
+    public GameObject planeF;
+
+    private bool playerIsOverlapping = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraA.enabled = false;
+        CameraB.enabled = false;
+        CameraC.enabled = false;
+        CameraD.enabled = false;
+        CameraE.enabled = false;
+        CameraF.enabled = true;
+
+        planeA.SetActive(false);
+        planeB.SetActive(false);
+        planeC.SetActive(false);
+        planeD.SetActive(false);
+        planeE.SetActive(false);
+        planeF.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+            Debug.Log(" Player is in Third area");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+            Debug.Log(" Player has left the Third area");
+        }
     }
 }
