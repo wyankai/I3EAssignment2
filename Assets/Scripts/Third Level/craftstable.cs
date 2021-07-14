4288babde6ee206b296a2a25fc873264684e8dbf
wyankai <73118690+wyankai@users.noreply.github.com>
2021-07-15 07:41:11 +0800

    Remove unnecessary UI
    Remove the Gate UI because it wasn't used

--- Assets/Scripts/Third Level/craftstable.cs
+++ Assets/Scripts/Third Level/craftstable.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class craftstable : MonoBehaviour
 {
     private int partsCollected = 0;
-    // Start is called before the first frame update
+    private int partsLeftToCollect;
+
+    // This is for adding the dialoguePanel and dialogueText so that the player can set it active when interacting with the craftstable
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+
+    private void Start()
+    {
+        //dialoguePanel.SetActive(false);
+        //dialogueText.gameObject.SetActive(false);
+    }
+
     public void addSwordParts()
     {
         partsCollected += 1;
         Debug.Log("Part Collection :" + partsCollected);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        partsLeftToCollect = 5 - partsCollected;
+        if (partsLeftToCollect > 0)
+        {
+            //dialogueText.text = "You have not collected all the parts yet. Parts left to collect : " + partsLeftToCollect;
+
+        }
+        else
+        {
+
+        }
     }
 }
